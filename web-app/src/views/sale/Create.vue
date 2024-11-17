c048e1cc9784f37f98a7ad7b715bfa5525d1740d
xieyangyuyue <1483618794@qq.com>
2024-11-17 16:40:55 +0800

    [add]添加表单校验

--- web-app/src/views/sale/Create.vue
+++ web-app/src/views/sale/Create.vue
@@ -2,33 +2,33 @@
   <div class="main">
     <a-steps :current="current">
       <a-step title="填写信息"></a-step>
-      <a-step title="确认信息"/>
-      <a-step title="完成"/>
+      <a-step title="确认信息" />
+      <a-step title="完成" />
     </a-steps>
     <div class="steps-content">
       <div v-if="current === 0">
-        <a-form-model :model="form" :label-col="labelCol" :wrapper-col="wrapperCol">
-          <a-form-model-item label="公司名称" required>
-            <a-input v-model="form.company"/>
+        <a-form-model :model="form" :rules="rules" :label-col="labelCol" :wrapper-col="wrapperCol" ref="formRef">
+          <a-form-model-item label="公司名称" prop="company">
+            <a-input v-model="form.company" />
           </a-form-model-item>
-          <a-form-model-item label="打款账号" required>
-            <a-input v-model="form.number"/>
+          <a-form-model-item label="打款账号" prop="number">
+            <a-input v-model="form.number" />
           </a-form-model-item>
-          <a-form-model-item label="售出商品" required>
-            <a-select v-model="selectIndex" placeholder="请选择商品">
+          <a-form-model-item label="售出商品" prop="selectIndex">
+            <a-select v-model="form.selectIndex" placeholder="请选择商品">
               <a-select-option :value="index" v-for="(item, index) in commodityList" :key="index">
                 {{ item.name }}
               </a-select-option>
             </a-select>
           </a-form-model-item>
-          <a-form-model-item label="商品数量" required>
-            <a-input-number v-model="form.count"/>
+          <a-form-model-item label="商品数量" prop="count">
+            <a-input-number v-model="form.count" />
           </a-form-model-item>
-          <a-form-model-item label="预留电话" required>
-            <a-input v-model="form.phone"/>
+          <a-form-model-item label="预留电话" prop="phone">
+            <a-input v-model="form.phone" />
           </a-form-model-item>
-          <a-form-model-item label="备注信息" required>
-            <a-input v-model="form.description" type="textarea" :rows="4"/>
+          <a-form-model-item label="备注信息" prop="description">
+            <a-input v-model="form.description" type="textarea" :rows="4" />
           </a-form-model-item>
           <a-form-model-item :wrapper-col="{ span: 14, offset: 6 }">
             <a-button type="primary" @click="next">
@@ -36,6 +36,7 @@
             </a-button>
           </a-form-model-item>
         </a-form-model>
+
       </div>
       <div v-if="current === 1" class="check">
         <p>收货公司： {{ form.company }}</p>
@@ -51,19 +52,15 @@
         <a-button @click="current = 0">上一步</a-button>
       </div>
       <div v-if="current === 2">
-        <a-result
-            status="success"
-            title="Submitted Successfully"
-            sub-title="Please wait for the administrator to review the delivery request."
-        >
+        <a-result status="success" title="提交成功" sub-title="请等待管理员审核交付请求.">
           <template #extra>
             <router-link to="/sale/record">
               <a-button key="console" type="primary">
-                Go Back
+                返回
               </a-button>
             </router-link>
             <a-button key="buy" @click="current = 0">
-              Submit Again
+              再次提交
             </a-button>
           </template>
         </a-result>
@@ -73,15 +70,15 @@
 </template>
 
 <script>
-import {FindAllCommodity} from "../../api/commodity";
-import {SaveSale} from "../../api/sale";
+import { FindAllCommodity } from "../../api/commodity";
+import { SaveSale } from "../../api/sale";
 
 export default {
   data() {
     return {
       loading: false,
-      labelCol: {span: 6},
-      wrapperCol: {span: 12},
+      labelCol: { span: 6 },
+      wrapperCol: { span: 12 },
       current: 0,
       selectIndex: 0,
       drivers: [],
@@ -96,6 +93,19 @@ export default {
         phone: '',
         description: '',
       },
+      rules: {
+        company: [{ required: true, message: "请输入公司名称", trigger: "blur" }],
+        number: [{ required: true, message: "请输入打款账号", trigger: "blur" }],
+        selectIndex: [{ required: true, message: "请选择商品", trigger: "change" }],
+        count: [{ required: true, message: "请输入商品数量", trigger: "blur" }],
+        phone: [
+          { required: true, message: "请输入预留电话", trigger: "blur" },
+          { pattern: /^1[3-9]\d{9}$/, message: "请输入有效的手机号", trigger: "blur" },
+        ],
+        description: [
+          { required: true, message: "请输入备注信息", trigger: "blur" },
+        ],
+      },
     }
   },
 
@@ -106,13 +116,28 @@ export default {
   },
 
   methods: {
+    // next() {
+    //   let commodity = this.commodityList[this.selectIndex]
+    //   this.form.price = this.form.count * commodity.price
+    //   this.form.commodity = commodity.name
+    //   console.log(this.form)
+    //   this.current = 1
+    // },
     next() {
-      let commodity = this.commodityList[this.selectIndex]
-      this.form.price = this.form.count * commodity.price
-      this.form.commodity = commodity.name
-      console.log(this.form)
-      this.current = 1
+      this.$refs.formRef.validate((valid) => {
+        if (valid) {
+          this.$message.success("验证通过，进入下一步！");
+          let commodity = this.commodityList[this.selectIndex]
+          this.form.price = this.form.count * commodity.price
+          this.form.commodity = commodity.name
+          console.log(this.form)
+          this.current = 1
+        } else {
+          this.$message.error("请修正表单中的错误！");
+        }
+      });
     },
+
     submit() {
       this.loading = true
       SaveSale(this.form).then((res) => {
@@ -159,5 +184,4 @@ export default {
   letter-spacing: 1px;
   color: red;
 }
-
 </style>
